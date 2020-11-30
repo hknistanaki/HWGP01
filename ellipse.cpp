266d57d31e36ea5077b11d50d158e94e87e44d95
hknistanaki <hknistanaki@gmail.com>
2020-11-30 14:22:24 -0800

    added eclipse.cpp

--- ellipse.cpp
+++ ellipse.cpp
@@ -1,2 +1,18 @@
-#include "shape.h"
-#include "ellipse.h"
+#include "Shape.h"
+#include "Ellipse.h"
+
+void Ellipse::set_point(const QPoint& point)
+{
+    points.push_back(point);
+}
+
+void Ellipse::draw(const int translate_x, const int translate_y)
+{
+    get_qpainter().setPen(get_pen());
+    get_qpainter().setBrush(get_brush());
+
+    get_qpainter().save();
+    get_qpainter().translate(translate_x, translate_y);
+
+    get_qpainter().restore();
+}
