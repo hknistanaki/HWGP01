ab5b9820acac4ae4b5cd9bc60fcd40cc8a883a23
hknistanaki <hknistanaki@gmail.com>
2020-11-30 14:23:14 -0800

    removed duplicate polyline

--- Line.h
+++ Line.h
@@ -6,7 +6,7 @@
 class Line : public Shape
 {
 public:
-    Line(int front, int end) : Shape{device, id, ShapeType::Line} {}
+    Line(QPaintDevice* device = nullptr, int id = -1) : Shape{device, id, ShapeType::Line} {}
     ~Line() override { }
 
     void set_points(const QPoint& point_begin, const QPoint& point_end);
@@ -16,20 +16,6 @@ public:
 private:
     QPoint point_begin;
     QPoint point_end;
-};
-
-class Polyline : public Shape
-{
-public:
-    Polyline(QPaintDevice* device = nullptr, int id = -1) : Shape{device, id, ShapeType::Polyline} {}
-    ~Polyline() override { }
-
-    void set_point(const QPoint& point);
-
-    void draw(const int translate_x = 0, const int translate_y = 0) override;
-
-private:
-    vector<QPoint> points;
 };
 
 #endif // LINE_H
