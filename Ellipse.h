c7534dbbbb9aa2c34e5b58a68c23353e223d1bb8
hknistanaki <hknistanaki@gmail.com>
2020-11-30 14:22:41 -0800

    added set_point and draw

--- Ellipse.h
+++ Ellipse.h
@@ -9,9 +9,13 @@ public:
     Ellipse(QPaintDevice* device = nullptr, int id = -1) : Shape{device, id, ShapeType::Ellipse} {}
     ~Ellipse() override { }
 
+    void set_point(const QPoint& point);
+
+    void draw(const int translate_x = 0, const int translate_y = 0) override;    
+
 private:
-
-
+    vector<QPoint> points;
+    
 };
 
 #endif // ELLIPSE_H
