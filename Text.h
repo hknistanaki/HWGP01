8f38dbb474fde1221b0dc286ab3a67b42a17f26e
hknistanaki <hknistanaki@gmail.com>
2020-12-01 10:50:18 -0800

    fixed typos

--- Text.h
+++ Text.h
@@ -9,8 +9,12 @@ public:
     Text(QPaintDevice * device = nullptr, int id = -1) : Shape{device, id, ShapeType::Text} { }
     ~Text() override { }
 
-private:
+    void set_point(const QPoint& point);
 
+    void draw(const int translate_x = 0, const int translate_y = 0) override;    
+
+private:
+    vector<QPoint> points;
 
 };
 
